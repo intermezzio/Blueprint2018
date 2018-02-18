0b9c35dfbe466e099117da5c7032dbe972ad4b92
Jack Madden <jackamadden@gmail.com>
2018-02-18 10:38:40 -0500

    Added WordnikSetup file

---
+++ WordnikSetup.py
@@ -0,0 +1,8 @@
+from wordnik import *
+Class WordnikSetup():
+	def __init__(self):
+		self.apiUrl = 'http://api.wordnik.com/v4'
+		self.apiKey = 'f2269092144bc2689a0080d8f3702e7ae6d3d8e56c6a4a031'
+		self.client = swagger.ApiClient(apiKey, apiUrl)
+	def getApiThing(self):
+		return self.client
