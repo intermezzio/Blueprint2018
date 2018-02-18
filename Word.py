5c7acef310037b9420cf42b2280d22416e8b67f4
Jack Madden <jackamadden@gmail.com>
2018-02-18 14:17:00 -0500

    Word.py in semifinal state, may add n-gram later

--- Word.py
+++ Word.py
@@ -1,16 +1,15 @@
-import requests
 from wordnik import *
-
 class Word:
-	def __init__(self, word):
+	def __init__(self, word, partOfSpeech):
 		self.wordName = word
 		self.partOfSpeech = None
 		self.etymology = None
-		self.lettersIn = []
+		self.lettersIn = self.wordName.split()
 		self.definition = None
 		self.wordApi = WordApi.WordApi(self.setupWordnik())
-	def getWordInfo(self):
-		defArray = self.wordApi.getDefinitions(self.wordName)
+		self.vocabTypes = {"JJ":"adjective", "JJR":"adjective","JJS":"adjective","NN":"noun","NNP":"noun","NNS":"noun","RB":"adverb","RBR":"adverb","RBS":"adverb","VB":"verb","VBD":"verb","VBG":"verb","VBN":"verb","VBP":"verb","VBZ":"verb"}
+	def getWordDefinition(self):
+		defArray = self.wordApi.getDefinitions(self.wordName, partOfSpeech=self.partOfSpeech, limit="3")
 		defArray = defArray[0]
 		self.definition = defArray.text
 		return 0
@@ -19,10 +18,13 @@ class Word:
 		apiKey = 'f2269092144bc2689a0080d8f3702e7ae6d3d8e56c6a4a031'
 		client = swagger.ApiClient(apiKey, apiUrl)
 		return client
+	def nltkPosHandler():
+		if self.partOfSpeech in self.vocabTypes and len(self.wordName) > 8:
+			self.partOfSpeech = self.vocabtypes[self.partOfSpeech]
+			self.getWordDefinition()
+		else:
+			self.definition = None
 	def getWord(self):
 		return self.wordName
-word = Word("plant")
-word.getWordInfo()
-print word.definition
 
 
