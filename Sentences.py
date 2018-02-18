d400b4c6d2af9044517115e9a94c92fb412847d5
Andrew M <anmascillaro@ctemc.org>
2018-02-18 14:18:54 -0500

    Add gitignore and Update Sentences.py

--- Sentences.py
+++ Sentences.py
@@ -1,13 +1,12 @@
 """
 List of Words in a sentence
 
-Credit to Bird, Steven, Edward Loper and Ewan Klein (2009), Natural Language Processing with Python. O’Reilly Media Inc.
 	for the nltk Library (Natural Language Toolkit Python)
 
 """
-from Word import *
+#from Word import *
 from wordnik import *
-from nltk import *
+import nltk
 from collections import Counter
 
 class Sentence:
@@ -16,7 +15,7 @@ class Sentence:
         self.words = nltk.word_tokenize(sentence)
         self.pos_words = None
 
-    def partOfSpeech(self, word)
+    def partOfSpeech(self, word):
     	pass
 
     def countOccurrences(self, word):
@@ -31,7 +30,30 @@ class Sentence:
     def combinePropers(self):
     	if self.pos_words is None:
     		self.getPartOfSpeech()
+    	print self.pos_words
+    	newSentence = []
+    	currWord = ""
+    	prevpos = ""
+    	for word, pos in self.pos_words:
+    		print word + " " + pos
+    		if pos in ("NNP"):
+    			if prevpos in ("NNP"):
+    				currWord += " " + word
+    			#elif i == len(self.pos_words) - 1:
+    			#	newSentence += [(word, pos)]
+    			#	currWord = word
+    			#	currWord = ""
+    			else:
+    				currWord = word
+    		else:
+    			if prevpos in ("NNP"):
+    				newSentence += [(currWord, "NNP")]
+    				currWord = ""
+    			else:
+    				newSentence += [word, pos]
+    		prevpos = pos
+    	print newSentence
 
-    	for wordTuple in self.pos_words:
-    		if wordTuple[1] == "NNP":
-    			pass
+firstSentence = Sentence("This is a random sentence sent from New Jersey")
+firstSentence.getPartOfSpeech()
+firstSentence.combinePropers()
