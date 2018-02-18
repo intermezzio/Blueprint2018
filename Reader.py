0594acf581e4758c63318d88c982a75783a35b6b
Andrew M <anmascillaro@ctemc.org>
2018-02-18 17:02:32 -0500

    Update Reader.py
    Still doesn't make a csv

--- Reader.py
+++ Reader.py
@@ -3,10 +3,28 @@ import re
 class Reader():
 	def __init__(self, text):
 		self.sentences = re.split(".|!|;|?", text)
-		self.sentenceArray = None
-		i = 0
+		self.sentenceArray = []
+		self.makeSentences()
+		self.masterDict = dict()
+
+	def makeSentences(self):
 		for sentence in self.sentences:
-			self.sentenceArray[i] = Sentence(sentence)
-			i+=1
-	def fileRead():
+			self.sentenceArray.append(Sentence(sentence))
+
+	def getMasterDictionary(self):
 		
+		for sentence in sentenceArray:
+			sentenceDict = sentence.getDictionary()
+			for word in sentenceDict.getKeys():
+				self.masterDict[word] = sentenceDict[word]
+
+		return self.masterDict
+
+
+	def getCSV(self):
+		csvFile = fopen("parsedSolution.csv", 'w')
+		for vocab in self.getMasterDictionary.getKeys():
+
+
+	def fileRead():
+		pass		
